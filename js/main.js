98f5974deb6a54bd9872490f30031edd08130798
Laura Lima <laura.lima@escolar.ifrn.edu.br>
2023-02-01 11:03:26 -0300

    Aula 01 - JavaScript

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var nome = "Laura de Lima Camilo";
+var idade = 19;
+var idade2 = 10;
+var frase = "O Japão é o melhor time do mundo";
+var lista = ["pera", " uva", " maçã"]
+var lista2 = ["vermelho", "azul", "verde", "amarelo"];
+
+alert("Meu primeiro JS");
+alert("Bem-vindo, " + nome);
+alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);
+console.log(nome + " tem " + idade);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+lista.push(" melão");
+lista.pop();
+alert(lista);
+console.log(lista);
+console.log(lista2);
